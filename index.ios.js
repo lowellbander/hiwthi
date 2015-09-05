fcad146a4cc724b8bdf1b7c3c0ddc56156a5ceeb
Akshay Bakshi <bakshiakshay94@gmail.com>
2015-09-05 13:43:05 -0400

    Profile view

--- index.ios.js
+++ index.ios.js
@@ -4,50 +4,130 @@
  */
 'use strict';
 
+var imageLink = 'https://pbs.twimg.com/profile_images/626930521365327873/sSJzUuI__400x400.jpg';
+var Button = require('react-native-button');
 var React = require('react-native');
 var {
   AppRegistry,
   StyleSheet,
   Text,
+  Image,
   View,
 } = React;
 
 var hiwthi = React.createClass({
   render: function() {
+    var tabBar = <View style={styles.tabContainer}>
+          <View style={styles.momentsTab}>
+            <Text>Moments</Text>
+          </View>
+          <View style={styles.needsTab}>
+            <Text>Needs</Text>
+          </View>
+        </View>
+    if (true) {
+      tabBar = null;
+    }
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View style={styles.frameContainer}>
+        <View style={styles.statusBarContainer}>
+
+        </View>
+        <View style={styles.headerContainer}>
+          <Image 
+            source={{uri: imageLink}}
+            style= {styles.profilePhoto}/>
+          <View style={styles.infoContainer}>
+            <Text style={styles.nameText}>
+            Nat Doe
+            </Text>
+            <Text style={styles.xMomentsText}>
+            19 Moments
+            </Text>
+            
+            <Button style={styles.button} onPress={this._handlePress}>
+        Follow
+      </Button>
+            
+          </View>
+        </View>
+        {tabBar}
+        <View style={styles.feedContainer}>
+        </View>
       </View>
     );
   }
 });
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  
+  frameContainer: {
+    flex:1,
+    backgroundColor:'#F7F7F7'
+  },
+  statusBarContainer: {
+    flex:1,
+    backgroundColor:'#00FFFF',
+  },
+  headerContainer: {
+    flex: 5,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  tabContainer: {
+    flex: 2,
+    flexDirection: 'row',
+    backgroundColor: '#FFFF00',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  feedContainer: {
+    flex:20,
+    backgroundColor:'#A2A2A2'
   },
+  profilePhoto: {
+      flex: 3,
+      width: 100,
+      height: 100,
+      borderRadius: 50,
+      margin: 20,
+  },
+  infoContainer: {
+    flex:7,
+  },
+  nameText: {
+      fontSize: 32,
+      fontFamily: 'Avenir',
+      marginBottom: -5,
+  },
+  xMomentsText: {
+      fontSize: 18,
+      color: '#A2A2A2',
+  },
+  button:{
+      flex:1,
+      width: 85,
+      marginTop: 10,
+      color: '#007AFF',
+      fontSize: 18,
+      textAlign:'center',
+      borderWidth: 1,
+      borderRadius: 8,
+      borderColor: '#007Aff',
+      padding: 6,
+
+  },
+  momentsTab: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+  },
+  needsTab: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+  },
+
 });
 
 AppRegistry.registerComponent('hiwthi', () => hiwthi);
